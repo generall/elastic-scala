3c156f97a85b482b99bf266a98ffdecf9456a05d
Andrey Vasnetsov <vasnetsov93@gmail.com>
2017-03-13 13:54:30 +0300

    Remove unused code

--- src/main/scala/ml/generall/elastic/MentionSearcher.scala
+++ src/main/scala/ml/generall/elastic/MentionSearcher.scala
@@ -13,21 +13,6 @@ import scala.collection.JavaConversions._
   * Created by generall on 22.08.16.
   */
 
-
-class MentionSearchResult(_vars: Iterable[ConceptVariant])(filterPredicate: (ConceptVariant => Boolean)) {
-  val stats: Iterable[ConceptVariant] = {
-    val avgs = _vars.map(_.avgScore)
-    val avgsNorm = ProbTools.normalize(avgs)
-    val avgsSotfMax = ProbTools.softMax(avgs)
-    _vars.zip(avgsNorm.zip(avgsSotfMax)).map({ case (variant, (norm, softMax)) => {
-      variant.avgSoftMax = softMax
-      variant.avgNorm = norm
-      variant
-    }
-    }).filter(filterPredicate)
-  }
-}
-
 class MentionSearcher(
                        val host: String,
                        val port: Int,
@@ -38,69 +23,6 @@ class MentionSearcher(
                      ) extends MentionSearcherAbs {
 
   var client: TcpClient = TcpClient.transport(ElasticsearchClientUri(host, port))
-
-
-  def filterResult(x: ConceptVariant): Boolean = {
-    if (x.count <= thresholdCount) {
-      if (isDebug()) println(s"Filter concept: ${x.concept}")
-      false
-    } else true
-  }
-
-  def calcStat(concept: String, list: Iterable[(String, Float)]): ConceptVariant = {
-    val size = list.size
-    ConceptVariant(
-      concept = concept,
-      count = size,
-      avgScore = list.foldLeft(0.0)(_ + _._2) / size,
-      maxScore = list.maxBy(_._2)._2,
-      minScore = list.minBy(_._2)._2
-    )
-  }
-
-  def findMentions(mentionText: String): MentionSearchResult = {
-    val resp = client.execute {
-      search(index / "mention") query {
-        matchQuery("anchor_text", mentionText)
-      } limit mentionLimit
-    }.await
-
-    val resScores = resp.to[String].zip(resp.hits.map(_.score))
-      .groupBy(_._1)
-      .map({ case (concept, pairs) => calcStat(concept, pairs) })
-
-    new MentionSearchResult(resScores)(filterResult)
-  }
-
-  def findHref(hrefToFind: String): List[Sentence] = {
-    val resp = client.execute {
-      search(index / "mention") query {
-        matchQuery("concept", hrefToFind)
-      } limit hrefLimit
-    }.await
-    resp.to[Mention].map(new Sentence(_)).toList
-  }
-
-  def findHrefWithContext(hrefToFind: String, leftContext: String, rightContext: String): List[Sentence] = {
-    val resp = client.execute {
-      search(index / "mention") query {
-        boolQuery().should(
-            matchQuery("context.left", leftContext),
-            matchQuery("context.right", rightContext)
-          ) filter termQuery("concept", hrefToFind)
-      } limit hrefLimit
-    }.await
-    resp.to[Mention].map(new Sentence(_)).toList
-  }
-
-  def innerFieldRequest(str: String): List[Sentence] = {
-    val resp = client.execute {
-      search(index / "mention") query {
-        matchQuery("context.right", str)
-      }
-    }.await
-    resp.to[Mention].map(new Sentence(_)).toList
-  }
 
   def customSearch[T](query: TcpClient => RichSearchResponse)(implicit hitas: HitReader[T], manifest: Manifest[T]): List[(T, Float)] = {
     val resp = query(client)
--- src/test/scala/MentionSearcherTest.scala
+++ src/test/scala/MentionSearcherTest.scala
@@ -9,27 +9,6 @@ import com.sksamuel.elastic4s.{ElasticClient, ElasticsearchClientUri, Hit, HitRe
   */
 class MentionSearcherTest extends FunSuite {
 
-  test("testFindMentions") {
-    val searcher = new MentionSearcher("localhost", 9300)
-    val res = searcher.findMentions("Titanic")
-    res.stats.foreach(println)
-  }
-
-  test("testFindHref") {
-    val searcher = new MentionSearcher("localhost", 9300)
-    searcher.findHref("http://en.wikipedia.org/wiki/Hot_air_balloon").foreach(x => println(x.sentence))
-  }
-
-  test("testFindHrefContext") {
-    val searcher = new MentionSearcher("192.168.1.44", 9300)
-    searcher.findHrefWithContext("http://en.wikipedia.org/wiki/RMS_Titanic", "", "iceberg").foreach(x => println(x.sentence))
-  }
-
-  test("testInnerFieldRequest") {
-    val searcher = new MentionSearcher("192.168.1.44", 9300)
-    val res = searcher.innerFieldRequest("Titanic").foreach(x => println(x.sentence))
-  }
-
   test("testCustomSearch") {
     val searcher = new MentionSearcher("localhost", 9300)
 
